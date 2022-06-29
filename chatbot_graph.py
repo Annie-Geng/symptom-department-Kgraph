656f5b1efea2551f2b799353be9856a6e8f0f438
Annie-Geng <gengsy@mail.ustc.edu.cn>
2022-06-29 18:58:50 +0800

    add sequential Q&A

--- chatbot_graph.py
+++ chatbot_graph.py
@@ -8,6 +8,8 @@ from question_classifier import *
 from question_parser import *
 from answer_search import *
 
+old_ans = []
+
 '''问答类'''
 class ChatBotGraph:
     def __init__(self):
@@ -16,21 +18,76 @@ class ChatBotGraph:
         self.searcher = AnswerSearcher()
 
     def chat_main(self, sent):
-        answer = '您好，我是分诊小助手，很抱歉没能听懂您的病症；今后我会更加完善功能，请您先咨询人工分诊医生哦！'
+        #answer = '您好，我是分诊小助手，很抱歉没能听懂您的病症；今后我会更加完善功能，请您先咨询人工分诊医生哦！'
+        answer = '请问您是否还有其他不适或症状？'
         res_classify = self.classifier.classify(sent)
         if not res_classify:
             return answer
         res_sql = self.parser.parser_main(res_classify)
-        final_answers = self.searcher.search_main(res_sql)
-        if not final_answers:
+
+        global old_ans
+        search_dic = self.searcher.search_main(res_sql,old_ans)
+        old_ans = search_dic['answers']
+        question_type = search_dic['question_type']
+        
+        chk = self.searcher.department_limitChk(old_ans)
+        if chk['flag']:
             return answer
         else:
+            final_answers = self.answer_prettify(question_type,chk['ans_all'])
             return '\n'.join(final_answers)
+    
+    '''根据对应的qustion_type, 调用相应的回复模板'''
+    def answer_prettify(self, question_type, answers):
+        final_answer = []
+        
+        #sorted_ans = sorted(answers, key= lambda x: x['get_prob'],reverse = True)
+        sorted_ans = answers
+        #final_ans = ''
+        if question_type == 'disease_department':
+            #TODO: 应该按可能性排序，而不是直接set
+            for ans in sorted_ans:
+                half_ans = ''
+                final_ans = ''
+                for dis in ans["diseases"]:
+                    half_ans += '您患“{0}”(患病率：{1}) '.format(dis['dis_name'], dis['get_prob'])
+                final_ans += half_ans
+                final_ans += '推荐去“{0}”就诊。\n'.format(ans['department'])
+                final_answer.append(final_ans)
+                
+        elif question_type == 'symptom_department':
+            
+            for ans in sorted_ans:
+                half_ans = ''
+                final_ans = ''
+                for dis in ans["diseases"]:
+                    half_ans += '您可能患“{0}”(患病率：{1}) '.format(dis['dis_name'], dis['get_prob'])
+                final_ans += half_ans
+                final_ans += '推荐去“{0}”就诊。\n'.format(ans['department'])
+                final_answer.append(final_ans)
+
+        return final_answer
 
 if __name__ == '__main__':
     handler = ChatBotGraph()
     while 1:
-        question = input('患者:')
-        answer = handler.chat_main(question)
-        print('分诊小助手:', answer)
+        print('您好，我是分诊小助手。请说明您的不适症状或所患疾病，我会帮您自动分诊！')
+        count = 0
+        flag = 1
+        old_ans = []
+        while flag:
+            question = input('患者:')
+            answer = handler.chat_main(question)
+            if answer == '请问您是否还有其他不适或症状？':
+                if count > 4:
+                    print('分诊小助手:很抱歉没能听懂您的病症；今后我会更加完善功能，请您先咨询人工分诊医生哦！\n')
+                    flag = 0
+                else:
+                    print('分诊小助手:', answer[:-1])
+                    count += 1
+            else:
+                print('分诊小助手:', answer[:-1])
+                print('如仍有其他问题，请联系工作医生！\n')
+                flag = 0
+            
 
